416ec061eedaaf45213c432d6fe9f21e458a1089
Valentijn de Jong <v.m.t.dejong-2@umcutrecht.nl>
2017-07-21 11:53:50 +0200

    added mModelPerformance
    Not debugged for mnlogit yet.

--- R/mPerformance.R
+++ R/mPerformance.R
@@ -1,6 +1,5 @@
 #' Performance of multinomial predictions.
 #'
-#'
 #' @description Predictive performance for predicted or fitted values. Requires the predicted
 #' (or fitted) probability matrix \code{p}, and one of the following: \code{labels},
 #' \code{indices} or \code{indicator.matrix}. Preferably one of the former.
@@ -12,12 +11,11 @@
 #' @template ref_Brier
 #' @template ref_Hand
 #'
-#' @return  Provides several types of performance measures: discrimination (M-index or multiclass AUC),
-#' overall performance (Brier score and various R squares: Cox, Nagelkerke and McFadden) and calibration ().
+#' @template return_mPerformance
 #'
 #' @export
 
-predictionsPerformance <- function(p, labels, indices, indicator.matrix, names = colnames(p), na.rm = T)
+mPerformance <- function(p, labels, indices, indicator.matrix, names = colnames(p), na.rm = T)
 {
   # labels <- getLabels(p = p, labels = labels, indices = indices, # Not (yet) necessary
   #                     indicator.matrix = indicator.matrix, names = names)
@@ -39,8 +37,60 @@ predictionsPerformance <- function(p, labels, indices, indicator.matrix, names =
   out
 }
 
-# mPerformance <- function()
-# {
-#
-# }
+
+getFitData <- function(fit, ...)
+{
+  if (any(class(fit) == "mlogit"))
+  {
+    p <- fit$probabilities
+    indicator.matrix <- fit$residuals + fit$fitted.values
+    return(list(p = p, indicator.matrix = indicator.matrix))
+  }
+
+  if (any(class(fit) == "mnlogit"))
+  {
+    p <- fit$probabilities
+    indicator.matrix <- fit$residuals + fit$fitted.values
+    return(list(p = p, indicator.matrix = indicator.matrix))
+    # Something goes wrong here. The likelihood is not correct. Probably conversion to indices goes wrong.
+  }
+
+  stop("class of fit object not recognized. Try mPerformance() instead.")
+}
+
+#' Performance of a multinomial prediction model
+#'
+#'
+#' @description Predictive performance for a prediction model. Works with the \code{mlogit}
+#' package.
+#'
+#' @param fit A model fit object.
+#'
+#' @template ref_Nagelkerke
+#' @template ref_Brier
+#' @template ref_Hand
+#'
+#' @template return_mPerformance
+#'
+#' @export
+
+mModelPerformance <- function(fit, ...)
+{
+  if (is.list(fit) && !is.data.frame(fit))
+  {
+    x <- getFitData(fit, ...)
+    p <- x$p
+    ## not necessary yet:
+    # labels <- getLabels(p, labels = x$labels, indices = x$indices, indicator.matrix = indicator.matrix)
+    indices          <- getIndices(        p = p, labels = x$labels, indices = x$indices,
+                                           indicator.matrix = x$indicator.matrix)
+    indicator.matrix <- getIndicatorMatrix(p = p, labels = x$labels, indices = x$indices,
+                                           indicator.matrix = x$indicator.matrix)
+    remove(x)
+
+    mPerformance(p = p, labels = labels, indices = indices, indicator.matrix = indicator.matrix)
+  }
+  else
+    stop("mModelPerformance requires a model fit object. Try mPerformance() instead.")
+}
 
---
+++ man-roxygen/return_mPerformance.R
@@ -0,0 +1,2 @@
+#' @return An object of class "mPerformance", containing several types of performance measures: discrimination (M-index or multiclass AUC),
+#' overall performance (Brier score and various R squares: Cox, Nagelkerke and McFadden) and calibration ().
